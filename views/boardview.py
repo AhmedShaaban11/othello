fd66cd44fe84595d9b1f94886931227cfed04962
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2024-05-15 15:19:05 +0300

    Refactor code

--- views/boardview.py
+++ views/boardview.py
@@ -4,20 +4,19 @@ from time import sleep
 
 
 class BoardView:
-    def __init__(self, board, game, canvas, length=600):
+    def __init__(self, board, game, canvas, screen_length=600):
         self.canvas = canvas
         self.game = game
         self.board = board
-        self.length = length
-        self.tile_length = length / 8
-        self.radius = self.tile_length / 2 - 5
+        self.tile_length = screen_length / 8
+        self.length = screen_length
 
     def center(self, row, col):
         return col * self.tile_length + self.tile_length / 2, row * self.tile_length + self.tile_length / 2
 
     def draw(self):
         # Draw background
-        pygame.draw.rect(self.canvas, "#009067", (0, 0, self.length, self.length))
+        self.canvas.fill("#009067")
         # Draw lines
         for i in range(9):
             # Horizontal line
@@ -25,16 +24,16 @@ class BoardView:
             # Vertical line
             pygame.draw.line(self.canvas, "#000000", (0, i * self.tile_length), (self.length, i * self.tile_length), 1)
         # Draw discs
-        if type(self.board) is None:
-            return
+        radius = self.tile_length / 2 - 5
         for row in range(8):
             for col in range(8):
-                if self.board.grid[row][col] == Disc.BLACK:
-                    pygame.draw.circle(self.canvas, "#000000", self.center(row, col), self.radius)
-                elif self.board.grid[row][col] == Disc.WHITE:
-                    pygame.draw.circle(self.canvas, "#ffffff", self.center(row, col), self.radius)
-                elif self.board.grid[row][col] == Disc.GREY:
-                    pygame.draw.circle(self.canvas, "#888888", self.center(row, col), self.radius)
+                center_point = self.center(row, col)
+                if self.board.get(row, col) == Disc.BLACK:
+                    pygame.draw.circle(self.canvas, "#000000", center_point, radius)
+                elif self.board.get(row, col) == Disc.WHITE:
+                    pygame.draw.circle(self.canvas, "#ffffff", center_point, radius)
+                elif self.board.get(row, col) == Disc.GREY:
+                    pygame.draw.circle(self.canvas, "#888888", center_point, radius)
         pygame.display.update()
 
     def get_row_col(self, screen_x, screen_y):
